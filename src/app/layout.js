0c8614afa9a9618281e08ec4871b80d16fdec6c2
Omar Ureña Medina <omar-145@outlook.com>
2025-03-02 21:59:35 -0400

    Add: Etiquetas OpenGraph

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,16 @@ const patrickHand = Patrick_Hand({
 
 export const metadata = {
   title: "Pre Internado Médico UASD ─ Promoción 2025-20",
-  description: "Página oficial de la promoción de pre-internos del 2025-20 de la UASD",
+  description: "Página oficial de la promoción de pre-internos médicos del 2025-20 de la UASD",
+  author: 'Omar Ureña Medina',
+  openGraph: {
+    title: "Pre Internado Médico UASD ─ Promoción 2025-20",
+    description: "Página oficial de la promoción de pre-internos médicos del 2025-20 de la UASD",
+    type: "website",
+    locale: "es_DO",
+    url: "https://preinternado.vercel.app/",
+    site_name: "Pre Internado Médico UASD",
+  }
 };
 
 export default function RootLayout({ children }) {
